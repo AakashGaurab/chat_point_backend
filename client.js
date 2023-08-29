5aa650626ad61a1884dc5578258d07479aed268b
Aakash Gaurab <59801581+AakashGaurab@users.noreply.github.com>
2023-08-29 17:05:57 +0530

    Update client.js

--- client.js
+++ client.js
@@ -8,7 +8,7 @@ const username = urlParams.get("username")
 const room = urlParams.get("room")
 console.log(username, room);
 
-const socket = io("https://chat-app-7pse.onrender.com/", { transports: ["websocket"] });
+const socket = io("https://chat-backend-a17w.onrender.com/", { transports: ["websocket"] });
 
 socket.emit('joinRoom', { username, room })
 
